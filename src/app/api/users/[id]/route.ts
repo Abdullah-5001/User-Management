47f53c48b456edf8aa89b66761263453d519125b
Abdullah Mahmood <mohammadabdullah9940@gmail.com>
2026-07-19 21:01:49 +0500

    added delete and update endpoint and started implementing authentication

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -18,3 +18,51 @@ export const GET = async (
     },
   );
 };
+
+export const PUT = async (
+  request: Request,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  try {
+    const body = await request.json();
+    const { id } = await params;
+    const updatedData = await prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: body,
+    });
+    return NextResponse.json(
+      {
+        data: updatedData,
+        message: "You record has been updated successfully",
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: error },
+      {
+        status: 500,
+      },
+    );
+  }
+};
+
+export const DELETE = async (
+  request: Request,
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  const { id } = await params;
+  await prisma.user.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  return NextResponse.json({
+    status: 204,
+  });
+};
